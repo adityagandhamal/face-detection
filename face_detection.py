4edc437783fb758a74105ed86374ec0f28320ee7
adityagandhamal <61016383+adityagandhamal@users.noreply.github.com>
2020-09-18 17:14:34 +0530

    commit files

---
+++ face_detection.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+# Load Model
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# Loading image and converting to grayscale
+img = cv2.imread("messi5.jpg")
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Face Detection
+faces = face_cascade.detectMultiScale(gray_img, 1.1, 4)
+
+# Drawing Rectangle around detected faces
+for x, y, w, h in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+
+cv2.imshow("image", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
